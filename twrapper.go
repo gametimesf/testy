ae4c8a082d9757afd8c468f61bd4fbc017daf549
Andy Janata <andy.janata@gametime.co>
2022-03-14 18:37:49 -0700

    add t.Helper to wrapper

--- twrapper.go
+++ twrapper.go
@@ -12,34 +12,42 @@ type tWrapper struct {
 var _ TestingT = (*tWrapper)(nil)
 
 func (t tWrapper) Fail() {
+	t.Helper()
 	t.t.Fail()
 }
 
 func (t tWrapper) FailNow() {
+	t.Helper()
 	t.t.FailNow()
 }
 
 func (t tWrapper) Fatal(args ...interface{}) {
+	t.Helper()
 	t.t.Fatal(args...)
 }
 
 func (t tWrapper) Fatalf(format string, args ...interface{}) {
+	t.Helper()
 	t.t.Fatalf(format, args...)
 }
 
 func (t tWrapper) Errorf(format string, args ...interface{}) {
+	t.Helper()
 	t.t.Errorf(format, args...)
 }
 
 func (t tWrapper) Helper() {
+	// this probably doesn't actually work right since the call stack is incorrect
 	t.t.Helper()
 }
 
 func (t tWrapper) Log(args ...interface{}) {
+	t.Helper()
 	t.t.Log(args...)
 }
 
 func (t tWrapper) Logf(format string, args ...interface{}) {
+	t.Helper()
 	t.t.Logf(format, args...)
 }
 
